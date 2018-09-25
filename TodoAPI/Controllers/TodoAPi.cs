635c21607b7fa9ad0605a8367967a927ac545a2c
Tran Hoang Minh <angelsea951@gmail.com>
2018-09-25 14:20:18 +0700

    update token

--- TodoAPI/Controllers/TodoAPi.cs
+++ TodoAPI/Controllers/TodoAPi.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace TodoAPI.Controllers
@@ -14,14 +17,18 @@ namespace TodoAPI.Controllers
         [HttpPost("Token")]
         public ActionResult Token()
         {
+            var claimData = new[] { new Claim (ClaimTypes.Name, "username" ) };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("123456789123456"));
+            var singingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
             var token = new JwtSecurityToken(
                 issuer: "mysite.com",
                 audience: "mysite.com",
                 expires: DateTime.Now.AddMinutes(2),
-
+                claims: claimData,
+                signingCredentials: singingCredentials
                 );
 
-            return Ok("Hello");
+            return Ok(token);
         }
     }
 }
